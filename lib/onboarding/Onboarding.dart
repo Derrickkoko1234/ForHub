5e7dab2a3ffe9bc57c16886b2c0f8dd65cf0b20e
Derrick Koko <kokoderrick3@gmail.com>
2022-04-08 00:31:01 +0100

    Adding Intro Slider

---
+++ lib/onboarding/Onboarding.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatefulWidget {
+  const OnboardingPage({Key? key}) : super(key: key);
+
+  @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/splash/ToTheHub.dart
+++ lib/splash/ToTheHub.dart
@@ -9,10 +9,11 @@ class ToTheHubPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: const [
-          Center(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Center(
             child: Padding(
-              padding: EdgeInsets.all(28.0),
+              padding: EdgeInsets.only(top: 28.0),
               child: Image(
                 image: AssetImage(
                   'assets/images/tothehub.png',
@@ -20,7 +21,50 @@ class ToTheHubPage extends StatelessWidget {
               ),
             ),
           ),
-          Text('To The Hub', style: TextStyle(fontSize: 20),)
+          const Center(
+            child: Text(
+              'To The Hub',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(
+              left: 28.0,
+              right: 28.0,
+              top: 10,
+            ),
+            child: Text(
+              'Quis porttitor nullam mi in tellus quis commodo. Mattis dignissim curabitur sed cursus sed porttitor vel at iaculis. Eget varius posuere massa, amet eu, non libero. Laoreet arcu sem libero, non amet, erat risus eu, egestas. Augue nulla feugiat nulla vel. Aliquet non dictum amet fermentum nibh enim fames mauris praesent. Pulvinar augue semper egestas praesent tincidunt quis eu.',
+              style: TextStyle(
+                fontSize: 18,
+                height: 1.4,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 28.0,
+              right: 28.0,
+              top: 30,
+            ),
+            child: Row(
+              children: const [
+                Text(
+                  'Begin your journey',
+                  style: TextStyle(fontSize: 18),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Image(
+                    image: AssetImage('assets/images/vector.png'),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
